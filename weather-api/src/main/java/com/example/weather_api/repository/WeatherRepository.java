c1b2a0395814c81e1441dd3e886b45e770c7768b
Aravind <aravind19112003@gmail.com>
2024-11-05 12:58:48 +0530

    Add repo

---
+++ weather-api/src/main/java/com/example/weather_api/repository/WeatherRepository.java
@@ -0,0 +1,10 @@
+package com.example.weather_api.repository;
+
+import com.example.weather_api.model.Weather;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WeatherRepository extends JpaRepository<Weather,Integer> {
+
+}
