c1905fa1c8b3dee7a247f926227185f35731ab95
Aravind <aravind19112003@gmail.com>
2024-11-05 13:47:12 +0530

    Add Working external API conn

--- weather-api/src/main/java/com/example/weather_api/controller/WeatherController.java
+++ weather-api/src/main/java/com/example/weather_api/controller/WeatherController.java
@@ -1,18 +1,21 @@
 package com.example.weather_api.controller;
 
 import com.example.weather_api.service.WeatherService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class WeatherController {
 
-    private WeatherService service;
-    @GetMapping("/{city}")
-    public ResponseEntity<String> getWeather(@PathVariable String city) {
-        return new ResponseEntity<>(service.getWeatherByCity(city) , HttpStatus.OK);
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping("/weather/{city}")
+    public String getWeather(@PathVariable String city) {
+        return weatherService.getWeatherByCity(city);
     }
 }
--- weather-api/src/main/java/com/example/weather_api/service/WeatherService.java
+++ weather-api/src/main/java/com/example/weather_api/service/WeatherService.java
@@ -1,8 +1,20 @@
 package com.example.weather_api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class WeatherService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+//    private final String API_KEY = "your_api_key";
+    private final String BASE_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/pollachi/2024-11-05/2024-11-05?key=";
+
     public String getWeatherByCity(String city) {
-        return "";
+        String url = BASE_URL;
+        return restTemplate.getForObject(url, String.class);
     }
 }
