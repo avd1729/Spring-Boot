6bbb29c53ee456a72f6d7bf3b3ec99273386af3f
Aravind <aravind19112003@gmail.com>
2024-11-07 22:33:45 +0530

    Basic setup

---
+++ expenseTracker/src/main/java/com/example/expenseTracker/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.expenseTracker.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+}
---
+++ expenseTracker/src/main/java/com/example/expenseTracker/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.expenseTracker.repository;
+
+import com.example.expenseTracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
